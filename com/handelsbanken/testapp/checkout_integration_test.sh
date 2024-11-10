4fb9d5df024ef8f6ab7adb9305ecb35f06922326
Rune Fevang <rune.fevang@gmail.com>
2024-11-10 21:00:43 +0100

    Update integration test to reflect proper test data
    Fails for now, but hopefully starts working once the endpoint uses the
    fixed data.

--- com/handelsbanken/testapp/checkout_integration_test.sh
+++ com/handelsbanken/testapp/checkout_integration_test.sh
@@ -10,10 +10,13 @@ done
 
 function test_checkout() {
   result=$(curl localhost:8080/checkout -s -d "$1" -H 'Content-type:application/json')
+
+  # NOTE: This really should be checked with jq instead of grep, I just didn't want to
+  #       add a dependency on a command line tool less likely to be installed.
   if ! (echo "${result}" | grep -F "$2") then
     echo -e "Test with input '$1' failed.\nGot '${result}' when expecting '$2'"
     exit 1
   fi
 }
 
-test_checkout '[10, 13]' '{"price":32.4}'
+test_checkout '["001","002","001","004","003"]' '{"price":360}'
